6b03044d7a1c876d913df8080b38f90a4779aa4a
NgocDangVan <ngocdangvan98@gmail.com>
2024-06-11 09:01:19 +0700

    Add sort

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,6 +5,7 @@ import {MatCardModule} from '@angular/material/card';
 import { FlexModule } from '@angular/flex-layout';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     ProductListComponent
@@ -14,7 +15,8 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     FlexModule,
     MatSnackBarModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class ProductModule { }
